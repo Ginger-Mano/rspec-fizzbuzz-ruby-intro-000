a989040cb8e67d9a2311b16820b3a99a57a0d2bb
imperative-publisher-3811 <mcasimirian@gmail.com>
2020-02-28 18:12:07 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,11 @@
 def fizzbuzz
 
+if fizz_3 = fizzbuzz(3)
+  puts "Fizz"
+elsif fizz_5 = fizzbuzz(5)
+  puts "Buzz"
+elsif fizz_15 = fizzbuzz(15)
+  puts "Fizzbuzz"
+else fizz_4 = fizzbuzz(4)
+  puts "nil"   
 end
