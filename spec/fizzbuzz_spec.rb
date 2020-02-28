cb921851228bd7e9ceef329d0cb788f91d7faccf
imperative-publisher-3811 <mcasimirian@gmail.com>
2020-02-28 18:18:18 +0000

    Automatically backed up by Learn

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,5 +1,6 @@
 require "pry"
 require_relative './spec_helper.rb'
+binding.pry
 require_relative '../fizzbuzz.rb'# We are missing a 'require_relative' here. Check README.md for instructions and an explanation!
 
 describe "fizzbuzz" do
