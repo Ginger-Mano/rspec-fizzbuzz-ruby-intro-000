046386f926eaba18349048c64951541b05d6937e
imperative-publisher-3811 <mcasimirian@gmail.com>
2020-02-28 20:24:45 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,7 +4,7 @@ if integer % 3 == 0
   return "Fizz"
 elsif integer % 5 == 0
   return "Buzz"
-elsif integer % 3 && 5 == 0 !
+elsif integer % 3 == 0 && 5 == 0
   return "FizzBuzz"
 elsif integer % 4
   return "nil"
