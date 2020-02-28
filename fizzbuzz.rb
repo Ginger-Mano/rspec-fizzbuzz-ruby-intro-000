dbb6e1a95e02302fe02304e5c56ceb7635f024d3
imperative-publisher-3811 <mcasimirian@gmail.com>
2020-02-28 20:59:04 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(integer)
 
 if integer % 3 == 0 && 5 == 0
-  return "Fizz"
+  return "FizzBuzz"
 elsif integer % 5 == 0
   return "Buzz"
-elsif integer % 3 == 0 
-  return "FizzBuzz"
+elsif integer % 3 == 0
+  return "Fizz"
   end
 end
